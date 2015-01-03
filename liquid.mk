9fc4e60f7b7f4fd40970db22e132278ddba88309
Tony Malagisi <mtb3000gt@gmail.com>
2015-01-03 09:41:11 -0500

    Attempt google play services force close fix
    Change-Id: Ia9bfd62dec09e8dd4009c1d15bd3c021b0f35ffe

--- liquid.mk
+++ liquid.mk
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/liquid/config/common_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2bst \
     TARGET_DEVICE=d2bst \
-    BUILD_FINGERPRINT="samsung/d2bst/d2bst:4.4.2/KOT49H/L710TVPU0ANF4:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2bst-user 4.4.2 KOT49H L710TVPU0ANF4 release-keys"
+    BUILD_FINGERPRINT="samsung/d2lterefreshspr/d2lterefreshspr:4.4.2/KOT49H/L710TVPU0ANF4:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2lterefreshspr-user 4.4.2 KOT49H L710TVPU0ANF4 release-keys"
 
 PRODUCT_NAME := liquid_d2bst
 PRODUCT_DEVICE := d2bst
